b32d7f966e0151878f661e47e0bc0c4471a3c6c2
sarz2 <sara.abou@chasacademy.se>
2022-05-06 16:27:42 +0200

    fix geolocation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import "./App.css";
-import React from "react";
-import Weather from "./components/weather/weather";
-import Forecast from "./components/forecast/forecast";
+import React, { useEffect, useState } from "react";
+import Weather from "./components/weather";
+import Forecast from "./components/forecast";
+import { IconDisplay } from "./components/IconDisplay";
 
 function App() {
+  const [lat, setLat] = useState(0);
+  const [long, setLong] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function fetchPosition() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      },
+      (err) => setErrorMessage({ err })
+    );
+  }
+
+  useEffect(() => {
+    fetchPosition();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <h1> U07 React Weather App</h1>
       </header>
       <main>
-        <Weather />
-        {/* <Forecast /> */}
+        <Weather lat={lat} long={long} />
+        <Forecast lat={lat} long={long} />
       </main>
       <footer>Page created by Sara</footer>
     </div>
