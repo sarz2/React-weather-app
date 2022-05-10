66b90fbe39ee33b66c04277bd9aa0d29dcdce0ff
sarz2 <sara.abou@chasacademy.se>
2022-05-10 14:44:26 +0200

    add config for icon

--- src/components/IconDisplay.js
+++ src/components/IconDisplay.js
@@ -1,49 +1,85 @@
 import React from "react";
+import "./IconDisplay.css";
 
 const iconConfig = {
-  hot: {
-    iconName: "sun",
+  clear: {
+    src: "http://openweathermap.org/img/wn/01d.png",
+    alt: "clear sky",
+  },
+  few: {
+    src: "http://openweathermap.org/img/wn/02d.png",
+    alt: "few clouds",
+  },
+  scattered: {
+    src: "http://openweathermap.org/img/wn/03d.png",
+    alt: "scattered clouds",
+  },
+  broken: {
+    src: "http://openweathermap.org/img/wn/04d.png",
+    alt: "broken clouds",
+  },
+
+  shower: {
+    src: "http://openweathermap.org/img/wn/09d.png",
+    alt: "shower rains",
+  },
+  thunder: {
+    src: "http://openweathermap.org/img/wn/11d.png",
+    alt: "thunderstorm",
   },
   snow: {
-    iconName: "snowflake",
+    src: "http://openweathermap.org/img/wn/13d.png",
+    alt: "snow",
   },
   mist: {
-    iconName: "low vision",
+    src: "http://openweathermap.org/img/wn/50d.png",
+    alt: "mist",
   },
   rain: {
-    iconName: "tint",
-  },
-  cloud: {
-    iconName: "cloud",
+    src: "http://openweathermap.org/img/wn/10d.png",
+    alt: "rain",
   },
 };
 
 const getIcon = (description) => {
-  if (description == "clear sky") {
-    return "hot";
-  } else if (
-    description == "few clouds" ||
-    "scattered clouds" ||
-    "broken clouds"
-  ) {
-    return "cloud";
-  } else if (description == "shower rain" || "rain" || "thunderstorm") {
-    return "rain";
-  } else if (description == "snow") {
-    return "snow";
-  } else {
-    return "mist";
+  switch (description) {
+    case "clear sky":
+      return "clear";
+      break;
+    case "few clouds":
+      return "few";
+      break;
+    case "scattered clouds":
+      return "scattered";
+      break;
+    case "broken clouds":
+      return "broken";
+      break;
+    case "shower rain":
+      return "shower";
+      break;
+    case "rain" || "light rain":
+      return "rain";
+      break;
+    case "thunderstorm":
+      return "thunder";
+      break;
+    case "snow":
+      return "snow";
+      break;
+    case "mist":
+      return "mist";
+      break;
   }
 };
 
 export const IconDisplay = (props) => {
   const icon = getIcon(props.description);
-  const { iconName } = iconConfig[icon];
+  const { src, alt } = iconConfig[icon];
 
   return (
-    <div className="ui card">
-      <i className={`massive ${iconName} icon`}></i>
-      <div>{props.children}</div>
+    <div className="iconcontainer">
+      <img src={src} alt={alt}></img>
     </div>
   );
 };
